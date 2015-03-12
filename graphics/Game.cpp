8b28d3eab4409e0c9707a0402499e8460c138015
Lucas Fryzek <nercos7@gmail.com>
2015-03-12 15:21:41 -0400

    add window

---
+++ graphics/Game.cpp
@@ -0,0 +1,37 @@
+#include "Game.hpp"
+
+bool Game::init(){
+	screen.create(sf::VideoMode(800,600), title, sf::Style::Default);
+	return false;
+}
+
+void Game::handleInput(){
+	sf::Event e;
+	while(screen.pollEvent(e)){
+		if(e.type == sf::Event::Closed){
+			screen.close();
+		}
+	}
+}
+
+void Game::update(float dt){
+	//TOOD Update code
+}
+
+void Game::render(){
+	//TOOD rendering code
+}
+
+int Game::run(){
+	float dt = 0;
+	sf::Clock dtTimer;
+	while(screen.isOpen()){
+		handleInput();
+		update(dt);
+		screen.clear(sf::Color::Black);
+		render();
+		screen.display();
+		dt = dtTimer.restart().asSeconds();
+	}
+	return 0;
+}
