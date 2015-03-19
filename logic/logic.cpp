7e7014e28de23add24016e9e5c92ec75abb102c3
Caelin <caelink@gmail.com>
2015-03-19 15:28:17 -0400

    fix stuff

--- logic/logic.cpp
+++ logic/logic.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 Ship::Ship ( int type, float x_pos, float y_pos ) {
-  height = 30 * type; width = 15;
-  x = x_pos; y = y_pos; angle = 90;
-  speed = 5; damage = 0;
+    height = 30 * type; width = 15;
+    x = x_pos; y = y_pos; angle = 90;
+    speed = 5; damage = 0;
 }
 
 
--- logic/logic.hpp
+++ logic/logic.hpp
@@ -1,22 +1,33 @@
 #include <vector>
 
+class HitBox {
+    private:
+        float height, width, x, y;
+    public:
+        float angle;
+        hitBox ( float x, float y, float height, float width, x);
+        bool checkWithin(float x_given, float y_given);
+};
 
 class Ship {
     private:
-        float height, width, x, y, angle, speed, damage;
-
+        float speed, damage;
+        hitBox h;
     public:
         Ship ( int type, float x_pos, float y_pos );
         bool checkHit ( float hit_x, float hit_y, float rad );
+        void updateHitboxes(float x, float y, float angle);
         bool isDead ();
         ~Ship ();
 };
 
+
+// For now, standard Sea is: Sea mainSea(param1,...,paramn)
 class Sea {
     private:
-
     public:
         std::vector<Ship*> ships;
+        Sea (int x, int y);
         void update ();
 };
 
