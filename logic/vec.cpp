b1aede96e7249df7d80990fd4743b209e6a1756d
Lucas Fryzek <nercos7@gmail.com>
2015-03-31 16:51:45 -0400

    add standard vector functions

---
+++ logic/vec.cpp
@@ -0,0 +1,43 @@
+#include "vec.hpp"
+
+vec2 vec2::add(vec2 lhs, vec2 rhs){
+	vec2 out;
+	out.x = lhs.x + rhs.x;
+	out.y = lhs.y + rhs.y;
+	return out;
+}
+
+vec2 vec2::sub(vec2 lhs, vec2 rhs){
+	vec2 out;
+	out.x = lhs.x - rhs.x;
+	out.y = lhs.x - rhs.y;
+	return out;
+}
+
+vec2 vec2::scalarMult(vec2 vec, float scalar){
+	vec2 out;
+	out.x = scalar*vec.x;
+	out.y = scalar*vec.y;
+	return out;
+}
+
+float vec2::dot(vec2 lhs, vec2 rhs){
+	return lhs.x*rhs.x + lhs.y*rhs.y;
+}
+
+vec2 operator+(vec2 lhs, vec2 rhs){
+	return vec2::add(lhs, rhs);
+}
+
+vec2 operator-(vec2 lhs, vec2 rhs){
+	return vec2::sub(lhs, rhs);
+}
+
+float operator*(vec2 lhs, vec2 rhs){
+	return vec2::dot(lhs, rhs);
+}
+
+vec2 operator*(float scalar, vec2 vec){
+	return vec2::scalarMult(vec, scalar);
+}
+
--- logic/vec.hpp
+++ logic/vec.hpp
@@ -5,6 +5,16 @@ class vec2 {
     public:
         float x;
         float y;
+		static vec2 add(vec2 lhs, vec2 rhs);
+		static vec2 sub(vec2 lhs, vec2 rhs);
+		static vec2 scalarMult(vec2 vec, float scalar);
+		static float dot(vec2 lhs, vec2 rhs);
 };
+
+// Overload vec2 operators
+vec2 operator+(vec2 lhs, vec2 rhs);
+vec2 operator-(vec2 lhs, vec2 rhs);
+float operator*(vec2 lhs, vec2 rhs);
+vec2 operator*(float scalar, vec2 vec);
 
 #endif
