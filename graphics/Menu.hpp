4cd7da0c240b02d851f738843028f335be6f1e64
Caelin <caelink@gmail.com>
2015-04-09 15:55:00 -0400

    Defined textures for start and quit buttons

--- graphics/Menu.hpp
+++ graphics/Menu.hpp
@@ -1,9 +1,22 @@
 #ifndef MENU_HPP_
 #define MENU_HPP_
+#include <SFML/Graphics.hpp>
+
 
 class Menu {
     private:
         Menu();
+
+        std::string startLoc;
+        std::string quitLoc;
+
+        sf::Sprite start;
+        sf::Texture startTex;
+        sf::Sprite quit;
+        sf::Texture quitTex;
+
+        bool setQuit(std::string Texture);
+        bool setStart(std::string Texture);
     public:
 		void drawMenu();
 };
